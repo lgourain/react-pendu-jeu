52551a7d5775195e576ae02bcefee20f4317ebbe
Louis Gourain <louis-externe.gourain@enedis.fr>
2019-05-22 11:18:13 +0200

    creating structure

--- src/App.js
+++ src/App.js
@@ -1,10 +1,36 @@
 import React, { Component } from 'react';
 import './App.css';
 
+const alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("")
+
 class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      lettre: "",
+      lettresUtilises: "",
+      motADeviner: "",
+      motDevine: "",
+    }
+  }
+  handleClick(lettre) {
+    console.log(lettre, 'clicked')
+  }
   render() {
     return (
-      <div>
+      <div className="container">
+        <div className="masque">
+          <p>MASQUE TEST</p>
+        </div>
+        <div className="boutons">
+          {
+            alphabet.map((lettre, index) => {
+              return (
+                <button key={index} onClick={() => this.handleClick(lettre)}>{lettre}</button>
+              )
+            })
+          }
+        </div>
       </div>
     );
   }
