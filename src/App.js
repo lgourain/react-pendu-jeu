5601cc7a73c15f3be933eaf102f5101f4c81d606
Louis Gourain <louis-externe.gourain@enedis.fr>
2019-05-22 13:44:23 +0200

    adding computer display with underscore logic

--- src/App.js
+++ src/App.js
@@ -2,25 +2,61 @@ import React, { Component } from 'react';
 import './App.css';
 
 const alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("")
+const mots = [
+  "ANGLE",
+  "ARMOIRE",
+  "BANC",
+  "BUREAU",
+  "CABINET",
+  "CARREAU",
+  "CHAISE",
+  "CLASSE",
+  "CLEF",
+  "COIN",
+  "COULOIR",
+  "DOSSIER",
+  "EAU",
+  "ECOLE",
+  "ENTRER",
+  "ESCALIER",
+  "ETAGERE",
+  "EXTERIEUR",
+  "FENETRE",
+  "INTERIEUR",
+  "LAVABO",
+  "LIT",
+  "MARCHE",
+  "MATELAS",
+  "MATERNELLE",
+  "MEUBLE",
+  "MOUSSE",
+  "MUR",
+  "PELUCHE",
+  "PLACARD",
+  "PLAFOND",
+  "PORTE",
+]
 
 class App extends Component {
-  constructor() {
-    super()
-    this.state = {
-      lettre: "",
-      lettresUtilises: "",
-      motADeviner: "",
-      motDevine: "",
-    }
+
+  initialState = {
+    usedLetters: new Set(),
+    phrase: mots[ Math.floor(Math.random() * Math.floor(mots.length)) ],
+    score: 0,
   }
+
+  state = { ...this.initialState }
+
   handleClick(lettre) {
     console.log(lettre, 'clicked')
   }
+
   render() {
+    const { usedLetters, phrase } = this.state
     return (
       <div className="container">
         <div className="masque">
-          <p>MASQUE TEST</p>
+          <p>{computeDisplay(phrase, usedLetters)}</p>
         </div>
         <div className="boutons">
           {
@@ -33,7 +69,15 @@ class App extends Component {
         </div>
       </div>
     );
-  }
+  }  
+}
+
+// Produit une représentation textuelle de l’état de la partie,
+// chaque lettre non découverte étant représentée par un _underscore_.
+function computeDisplay(phrase, usedLetters) {
+  return phrase.replace(/\w/g,
+    (letter) => (usedLetters.has(letter) ? letter : '_')
+  )
 }
 
 export default App;
